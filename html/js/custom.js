2ea47e002a3f24e390fe4879c5579f4d129e9565
Sally Chen <23687599@student.uwa.edu.au>
2025-01-17 18:12:45 +0800

    contact section done

--- html/js/custom.js
+++ html/js/custom.js
@@ -11,3 +11,20 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+function sendEmail() {
+  var firstName = document.getElementById("firstname").value;
+  var lastName = document.getElementById("lastname").value;
+  var message = document.getElementById("message").value;
+
+  var subject =
+    "New Contact Form Submission from " + firstName + " " + lastName;
+  var body = message;
+
+  var mailtoLink =
+    "mailto:your@email.com?subject=" +
+    encodeURIComponent(subject) +
+    "&body=" +
+    encodeURIComponent(body);
+  window.location.href = mailtoLink;
+}
